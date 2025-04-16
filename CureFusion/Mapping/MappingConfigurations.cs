296036171fc82e1c22bab95faa2853ace4e612ea
Youssif Elish <Youssifelish@outlook.com>
2025-04-16 03:39:36 +0200

    Update Mapster Config

---
+++ CureFusion/Mapping/MappingConfigurations.cs
@@ -0,0 +1,14 @@
+﻿using CureFusion.Contracts.Appointment;
+using Mapster;
+
+
+namespace SurveyBasket.Mapping
+{
+    public class MappingConfigurations : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+                
+        }
+    }
+}
